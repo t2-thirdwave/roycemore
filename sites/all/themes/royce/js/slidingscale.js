018bb667f45b02a08fb11735c1b5026853833d0d
solutiondrop <scott@solutiondrop.com>
2017-01-26 12:53:24 -0600

    updated tuition javascript

--- sites/all/themes/royce/js/slidingscale.js
+++ sites/all/themes/royce/js/slidingscale.js
@@ -2,102 +2,75 @@ var tuition_data =
 {
   "Less than $60,000":
     [
-      ["pkhalf", 2, "2,042", "4,084", "The full tuition for 2016-2017 for PreK & JK 1/2 day is $10,210"],
-      ["pk3", 2, "3,280", "6,560", "The full tuition for 2016-2017 for PreK & JK to 3 is $16,400"],
-      ["kindergarten", 2, "3,690", "7,380", "The full tuition for 2016-2017 for Kindergarten is $18,450"],
-      ["12", 2, "3,880", "7,760", "The full tuition for 2016-2017 for Grades 1 & 2 is $19,400"],
-      ["34", 2, "4,326", "8,652", "The full tuition for 2016-2017 for Grades 3 & 4 is $21,630"],
-      ["56", 2, "4,970", "9,940", "The full tuition for 2016-2017 for Grades 5 & 6 is $24,850"],
-      ["78", 2, "5,340", "10,680", "The full tuition for 2016-2017 for Grades 7 & 8 is $26,700"],
-      ["912", 2, "5,660", "11,320", "The full tuition for 2016-2017 for Grades 9-12 is $28,300"]
+      ["pkjkhalf", 2, "2,092", "4,184", "The full tuition for 2017-2018 for PreK & JK 1/2 day is $10,460"],
+      ["pkjkkfull", 2, "3,530", "7,060", "The full tuition for 2017-2018 for PreK & JK to Kindergarten is $17,650"],
+      ["14", 2, "4,190", "8,380", "The full tuition for 2017-2018 for Grades 1-4 is $20,950"],
+      ["58", 2, "5,122", "10,244", "The full tuition for 2017-2018 for Grades 5-8 is $25,610"],
+      ["912", 2, "5,782", "11,564", "The full tuition for 2017-2018 for Grades 9-12 is $28,910"]
     ],
   "$60,000-75,000":
     [
-      ["pkhalf", 2, "4,084", "6,126", "The full tuition for 2016-2017 for PreK & JK 1/2 day is $10,210"],
-      ["pk3", 2, "6,560", "9,840", "The full tuition for 2016-2017 for PreK & JK to 3 is $16,400"],
-      ["kindergarten", 2, "7,380", "11,070", "The full tuition for 2016-2017 for Kindergarten is $18,450"],
-      ["12", 2, "7,760", "11,640", "The full tuition for 2016-2017 for Grades 1 & 2 is $19,400"],
-      ["34", 2, "8,652", "12,978", "The full tuition for 2016-2017 for Grades 3 & 4 is $21,630"],
-      ["56", 2, "9,940", "14,910", "The full tuition for 2016-2017 for Grades 5 & 6 is $24,850"],
-      ["78", 2, "10,680", "16,020", "The full tuition for 2016-2017 for Grades 7 & 8 is $26,700"],
-      ["912", 2, "11,320", "16,980", "The full tuition for 2016-2017 for Grades 9-12 is $28,300"]
+      ["pkjkhalf", 2, "4,184", "6,276", "The full tuition for 2017-2018 for PreK & JK 1/2 day is $10,460"],
+      ["pkjkkfull", 2, "7,060", "10,590", "The full tuition for 2017-2018 for PreK & JK to Kindergarten is $17,650"],
+      ["14", 2, "8,380", "12,570", "The full tuition for 2017-2018 for Grades 1-4 is $20,950"],
+      ["58", 2, "10,244", "15,366", "The full tuition for 2017-2018 for Grades 5-8 is $25,610"],
+      ["912", 2, "11,564", "17,346", "The full tuition for 2017-2018 for Grades 9-12 is $28,910"]
     ],
   "$75,000-90,000":
     [
-      ["pkhalf", 2, "4,084", "6,126", "The full tuition for 2016-2017 for PreK & JK 1/2 day is $10,210"],
-      ["pk3", 2, "6,560", "9,840", "The full tuition for 2016-2017 for PreK & JK to 3 is $16,400"],
-      ["kindergarten", 2, "7,380", "11,070", "The full tuition for 2016-2017 for Kindergarten is $18,450"],
-      ["12", 2, "7,760", "11,640", "The full tuition for 2016-2017 for Grades 1 & 2 is $19,400"],
-      ["34", 2, "8,652", "12,978", "The full tuition for 2016-2017 for Grades 3 & 4 is $21,630"],
-      ["56", 2, "9,940", "14,910", "The full tuition for 2016-2017 for Grades 5 & 6 is $24,850"],
-      ["78", 2, "10,680", "16,020", "The full tuition for 2016-2017 for Grades 7 & 8 is $26,700"],
-      ["912", 2, "11,320", "16,980", "The full tuition for 2016-2017 for Grades 9-12 is $28,300"]
+      ["pkjkhalf", 2, "4,184", "6,276", "The full tuition for 2017-2018 for PreK & JK 1/2 day is $10,460"],
+      ["pkjkkfull", 2, "7,060", "10,590", "The full tuition for 2017-2018 for PreK & JK to Kindergarten is $17,650"],
+      ["14", 2, "8,380", "12,570", "The full tuition for 2017-2018 for Grades 1-4 is $20,950"],
+      ["58", 2, "10,244", "15,366", "The full tuition for 2017-2018 for Grades 5 & 8 is $25,610"],
+      ["912", 2, "11,564", "17,346", "The full tuition for 2017-2018 for Grades 9-12 is $28,910"]
     ],
   "$90,000-105,000":
     [
-      ["pkhalf", 2, "4,084", "6,126", "The full tuition for 2016-2017 for PreK & JK 1/2 day is $10,210"],
-      ["pk3", 2, "6,560", "9,840", "The full tuition for 2016-2017 for PreK & JK to 3 is $16,400"],
-      ["kindergarten", 2, "7,380", "11,070", "The full tuition for 2016-2017 for Kindergarten is $18,450"],
-      ["12", 2, "7,760", "11,640", "The full tuition for 2016-2017 for Grades 1 & 2 is $19,400"],
-      ["34", 2, "8,652", "12,978", "The full tuition for 2016-2017 for Grades 3 & 4 is $21,630"],
-      ["56", 2, "9,940", "14,910", "The full tuition for 2016-2017 for Grades 5 & 6 is $24,850"],
-      ["78", 2, "10,680", "16,020", "The full tuition for 2016-2017 for Grades 7 & 8 is $26,700"],
-      ["912", 2, "11,320", "16,980", "The full tuition for 2016-2017 for Grades 9-12 is $28,300"]
+      ["pkjkhalf", 2, "4,184", "6,276", "The full tuition for 2017-2018 for PreK & JK 1/2 day is $10,460"],
+      ["pkjkkfull", 2, "7,060", "10,590", "The full tuition for 2017-2018 for PreK & JK to Kindergarten is $17,650"],
+      ["14", 2, "8,380", "12,570", "The full tuition for 2017-2018 for Grades 1-4 is $20,950"],
+      ["58", 2, "10,244", "15,366", "The full tuition for 2017-2018 for Grades 5-8 is $25,610"],
+      ["912", 2, "11,564", "17,346", "The full tuition for 2017-2018 for Grades 9-12 is $28,910"]
     ],
   "$105,000-120,000":
     [
-      ["pkhalf", 2, "6,126", "8,168", "The full tuition for 2016-2017 for PreK & JK 1/2 day is $10,210"],
-      ["pk3", 2, "9,840", "13,120", "The full tuition for 2016-2017 for PreK & JK to 3 is $16,400"],
-      ["kindergarten", 2, "11,070", "14,760", "The full tuition for 2016-2017 for Kindergarten is $18,450"],
-      ["12", 2, "11,640", "15,520", "The full tuition for 2016-2017 for Grades 1 & 2 is $19,400"],
-      ["34", 2, "12,978", "17,304", "The full tuition for 2016-2017 for Grades 3 & 4 is $21,630"],
-      ["56", 2, "14,910", "19,880", "The full tuition for 2016-2017 for Grades 5 & 6 is $24,850"],
-      ["78", 2, "16,020", "21,360", "The full tuition for 2016-2017 for Grades 7 & 8 is $26,700"],
-      ["912", 2, "16,980", "22,640", "The full tuition for 2016-2017 for Grades 9-12 is $28,300"]
+      ["pkjkhalf", 2, "6,126", "8,168", "The full tuition for 2017-2018 for PreK & JK 1/2 day is $10,460"],
+      ["pkjkkfull", 2, "10,590", "14,120", "The full tuition for 2017-2018 for PreK & JK to Kindergarten is $17,650"],
+      ["14", 2, "12,570", "16,760", "The full tuition for 2017-2018 for Grades 1-4 is $20,950"],
+      ["58", 2, "15,366", "20,488", "The full tuition for 2017-2018 for Grades 5-8 is $25,610"],
+      ["912", 2, "17,346", "23,128", "The full tuition for 2017-2018 for Grades 9-12 is $28,910"]
     ],
   "$120,000-135,000":
     [
-      ["pkhalf", 2, "6,126", "8,168", "The full tuition for 2016-2017 for PreK & JK 1/2 day is $10,210"],
-      ["pk3", 2, "9,840", "13,120", "The full tuition for 2016-2017 for PreK & JK to 3 is $16,400"],
-      ["kindergarten", 2, "11,070", "14,760", "The full tuition for 2016-2017 for Kindergarten is $18,450"],
-      ["12", 2, "11,640", "15,520", "The full tuition for 2016-2017 for Grades 1 & 2 is $19,400"],
-      ["34", 2, "12,978", "17,304", "The full tuition for 2016-2017 for Grades 3 & 4 is $21,630"],
-      ["56", 2, "14,910", "19,880", "The full tuition for 2016-2017 for Grades 5 & 6 is $24,850"],
-      ["78", 2, "16,020", "21,360", "The full tuition for 2016-2017 for Grades 7 & 8 is $26,700"],
-      ["912", 2, "16,980", "22,640", "The full tuition for 2016-2017 for Grades 9-12 is $28,300"]
+      ["pkjkhalf", 2, "6,126", "8,168", "The full tuition for 2017-2018 for PreK & JK 1/2 day is $10,460"],
+      ["pkjkkfull", 2, "10,590", "14,120", "The full tuition for 2017-2018 for PreK & JK to Kindergarten is $17,650"],
+      ["14", 2, "12,570", "16,760", "The full tuition for 2017-2018 for Grades 1-4 is $20,950"],
+      ["58", 2, "15,366", "20,488", "The full tuition for 2017-2018 for Grades 5-8 is $25,610"],
+      ["912", 2, "17,346", "23,128", "The full tuition for 2017-2018 for Grades 9-12 is $28,910"]
     ],
   "$135,000-150,000":
     [
-      ["pkhalf", 2, "8,168", "10,210", "The full tuition for 2016-2017 for PreK & JK 1/2 day is $10,210"],
-      ["pk3", 2, "13,120", "16,400", "The full tuition for 2016-2017 for PreK & JK to 3 is $16,400"],
-      ["kindergarten", 2, "14,760", "18,450", "The full tuition for 2016-2017 for Kindergarten is $18,450"],
-      ["12", 2, "15,520", "19,400", "The full tuition for 2016-2017 for Grades 1 & 2 is $19,400"],
-      ["34", 2, "17,304", "21,630", "The full tuition for 2016-2017 for Grades 3 & 4 is $21,630"],
-      ["56", 2, "19,880", "24,850", "The full tuition for 2016-2017 for Grades 5 & 6 is $24,850"],
-      ["78", 2, "21,360", "26,700", "The full tuition for 2016-2017 for Grades 7 & 8 is $26,700"],
-      ["912", 2, "22,640", "28,300", "The full tuition for 2016-2017 for Grades 9-12 is $28,300"]
+      ["pkjkhalf", 2, "8,368", "10,460", "The full tuition for 2017-2018 for PreK & JK 1/2 day is $10,460"],
+      ["pkjkkfull", 2, "14,120", "17,650", "The full tuition for 2017-2018 for PreK & JK to Kindergarten is $17,650"],
+      ["14", 2, "16,760", "20,950", "The full tuition for 2017-2018 for Grades 1-4 is $20,950"],
+      ["58", 2, "20,488", "25,610", "The full tuition for 2017-2018 for Grades 5-8 is $25,610"],
+      ["912", 2, "23,128", "28,910", "The full tuition for 2017-2018 for Grades 9-12 is $28,910"]
     ],
   "$150,000-165,000":
     [
-      ["pkhalf", 2, "8,168", "10,210", "The full tuition for 2016-2017 for PreK & JK 1/2 day is $10,210"],
-      ["pk3", 2, "13,120", "16,400", "The full tuition for 2016-2017 for PreK & JK to 3 is $16,400"],
-      ["kindergarten", 2, "14,760", "18,450", "The full tuition for 2016-2017 for Kindergarten is $18,450"],
-      ["12", 2, "15,520", "19,400", "The full tuition for 2016-2017 for Grades 1 & 2 is $19,400"],
-      ["34", 2, "17,304", "21,630", "The full tuition for 2016-2017 for Grades 3 & 4 is $21,630"],
-      ["56", 2, "19,880", "24,850", "The full tuition for 2016-2017 for Grades 5 & 6 is $24,850"],
-      ["78", 2, "21,360", "26,700", "The full tuition for 2016-2017 for Grades 7 & 8 is $26,700"],
-      ["912", 2, "22,640", "28,300", "The full tuition for 2016-2017 for Grades 9-12 is $28,300"]
+      ["pkjkhalf", 2, "8,368", "10,460", "The full tuition for 2017-2018 for PreK & JK 1/2 day is $10,460"],
+      ["pkjkkfull", 2, "14,120", "17,650", "The full tuition for 2017-2018 for PreK & JK to Kindergarten is $17,650"],
+      ["14", 2, "16,760", "20,950", "The full tuition for 2017-2018 for Grades 1-4 is $20,950"],
+      ["58", 2, "20,488", "25,610", "The full tuition for 2017-2018 for Grades 5 & 6 is $24,850"],
+      ["912", 2, "23,128", "28,910", "The full tuition for 2017-2018 for Grades 9-12 is $28,910"]
     ],
   "More than $165,000":
     [
-      ["pkhalf", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $10,210"],
-      ["pk3", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $16,400"],
-      ["kindergarten", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $18,450"],
-      ["12", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $19,400"],
-      ["34", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $21,630"],
-      ["56", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $24,850"],
-      ["78", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $26,700"],
-      ["912", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $28,300"]
+      ["pkjkhalf", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $10,460"],
+      ["pkjkkfull", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $17,650"],
+      ["14", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $20,950"],
+      ["58", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $25,610"],
+      ["912", 1, "Based on your specific family circumstances, you may qualify for less than full tuition of $28,910"]
     ]
 };
 
@@ -150,7 +123,7 @@ jQuery(document).ready(function($){
       }
     }
   });
-  
+
   $("#recalculate_tuition").click(function() {
     $(".tuition_form").show();
     $(".tuition_info").hide();
